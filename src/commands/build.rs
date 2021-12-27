eab0b145fe14d0377caff9b89870dbf91b58489e
Shobhit <shobhit@fifthtry.com>
2021-12-27 16:18:07 +0530

    Title evaluation and set

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -322,6 +322,11 @@ async fn process_ftd(
                     });
                 }
             };
+
+        let doc_title = match &main_ftd_doc.title() {
+            Some(x) => x.rendered.clone(),
+            _ => main.id.as_str().to_string(),
+        };
         let main_rt_doc = main_ftd_doc.to_rt("main", &main.id);
 
         let message_ftd_doc = match ftd::p2::Document::from("message", message, &lib) {
@@ -334,11 +339,13 @@ async fn process_ftd(
         };
         let message_rt_doc = message_ftd_doc.to_rt("message", &main.id);
 
+
         let mut f = tokio::fs::File::create(new_file_path).await?;
 
         f.write_all(
             fix_base(
                 fpm::with_message()
+                    .replace("__ftd_doc_title__", doc_title.as_str())
                     .replace(
                         "__ftd_data_message__",
                         serde_json::to_string_pretty(&message_rt_doc.data)
@@ -419,6 +426,11 @@ async fn process_ftd(
                 });
             }
         };
+
+        let doc_title = match &main_ftd_doc.title() {
+            Some(x) => x.rendered.clone(),
+            _ => main.id.as_str().to_string(),
+        };
         let message_rt_doc = message_ftd_doc.to_rt("message", &main.id);
 
         let fallback_ftd_doc =
@@ -437,6 +449,7 @@ async fn process_ftd(
         f.write_all(
             fix_base(
                 fpm::with_fallback()
+                    .replace("__ftd_doc_title__", doc_title.as_str())
                     .replace(
                         "__ftd_data_message__",
                         serde_json::to_string_pretty(&message_rt_doc.data)
