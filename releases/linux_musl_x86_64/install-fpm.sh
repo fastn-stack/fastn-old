3246bed910ac190f6ff752bbc597f3879d5080af
Shobhit <shobhit@fifthtry.com>
2022-01-06 14:48:09 +0530

    Update to tmp mode right now

--- releases/linux_musl_x86_64/install-fpm.sh
+++ releases/linux_musl_x86_64/install-fpm.sh
@@ -1,5 +1,6 @@
 # usr/bin/sh
 
-curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\ fpm_linux[A-z_]*" | cut -d : -f 2,3 | xargs -I % curl -L % -o /usr/bin/fpm
-curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\/fpm_linux.*.d" | cut -d : -f 2,3 | xargs -I % curl -L % -o /usr/bin/fpm.d
-chmod +x /usr/bin/fpm*
+mkdir -p /tmp/fpm/bin/
+curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\ fpm_linux[A-z_]*" | cut -d : -f 2,3 | xargs -I % curl -L % -o /tmp/fpm/bin/fpm
+curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\/fpm_linux.*.d" | cut -d : -f 2,3 | xargs -I % curl -L % -o /tmp/fpm/bin/fpm.d
+chmod +x /tmp/fpm/bin/fpm*
