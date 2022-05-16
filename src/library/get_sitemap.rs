ee05ffc40256e103112bfd30d460db05478dcd76
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-05-16 22:51:47 +0530

    Added current section, current subsection and current_page

--- src/library/get_sitemap.rs
+++ src/library/get_sitemap.rs
@@ -21,6 +21,7 @@ pub fn processor(
         }
 
         if let Some(sitemap) = sitemap.get_sitemap_by_id(doc_id.as_str()) {
+            dbg!(&sitemap);
             return doc.from_json(&sitemap, section);
         }
     }
