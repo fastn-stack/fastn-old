b40292f32414fe08373c4d2f0bcd82ac8b741442
Rithik Seth <106665190+Heulitig@users.noreply.github.com>
2022-06-08 17:49:40 +0530

    Used camino and avoided explicit use of unwrap()

--- src/commands/start_project.rs
+++ src/commands/start_project.rs
@@ -13,18 +13,33 @@ async fn write_file(file_name: &str, dir: &str, content: &str) -> fpm::Result<()
     Ok(())
 }
 
-pub async fn start_project(name: Option<&str>, path: Option<&str>) -> fpm::Result<()> {
-    let base_path = std::env::current_dir()?.to_str().unwrap().to_string();
+pub async fn start_project(name: &str, path: Option<&str>) -> fpm::Result<()> {
+
+    let base_path = {
+        match std::env::current_dir() {
+            Ok(bp) => match bp.to_str() {
+                Some(fbp) => fbp.to_string(),
+                None => "None".to_string(),
+            },
+            Err(_) => panic!("Error cannot find the current working directory!!"),
+        }
+    };
+
     // Not using config for base path as it requires manifest or FPM.ftd file for building and will throw error
     // and since this command should work from anywhere within the system
     // so we dont need to rely on config for using it
 
     // name is a required field so it will always be some defined string (cant be None)
-    // path is an optional field and has a default value "." for current working directory (also cant be None)
-    let (name, path) = (name.unwrap(), path.unwrap());
-    let final_dir = match path {
-        "." => format!("{}/{}", base_path, name),
-        _ => format!("{}/{}/{}", base_path, path, name),
+    // name can be any package url or standard project name
+    // path is an optional field and if no path is provided then current directory is used
+
+    let final_dir = {
+        match path {
+            Some(p) => camino::Utf8PathBuf::from(base_path)
+                .join(p)
+                .join(name),
+            None => camino::Utf8PathBuf::from(base_path).join(name),
+        }
     };
 
     // Create all directories if not present
@@ -34,8 +49,8 @@ pub async fn start_project(name: Option<&str>, path: Option<&str>) -> fpm::Resul
     let tmp_fpm = tmp_contents.0;
     let tmp_index = tmp_contents.1;
 
-    write_file("FPM.ftd", &final_dir, &tmp_fpm).await?;
-    write_file("index.ftd", &final_dir, &tmp_index).await?;
+    write_file("FPM.ftd", final_dir.as_str(), &tmp_fpm).await?;
+    write_file("index.ftd", final_dir.as_str(), &tmp_index).await?;
     println!(
         "Template FTD project created - {}\nPath -{}",
         name, final_dir
