960c7ceb1eadb8041af15c313eb7413c9ee92ccf
Heulitig <rithik@fifthtry.com>
2022-09-14 10:47:55 +0530

    using moved ftd parsing functions #2

--- src/config.rs
+++ src/config.rs
@@ -363,105 +363,10 @@ impl Config {
             Ok(())
         }
 
-        /// returns true when the next occurrence of id header needs to be ignored
-        /// for cases when the section-line refers to a component definition
-        /// or if the section is a youtube type component
-        /// since user can put multiple instances of this component
-        /// in various places in the package referring to the same youtube video
-        /// having same id so ignoring these ids.
-        ///
-        /// in any other case returns false
-        fn ignore_next_id(section_line: &str) -> bool {
-            // Strip out initial '-- ' or '--- '
-            let section_line = section_line.trim_start_matches('-').trim_start();
-
-            let before_caption = section_line.split_once(':').map(|s| s.0);
-            if let Some(section) = before_caption {
-                let mut parts = section.splitn(2, ' ');
-
-                // in case of component definition, section-kind will be mandatory
-                // -- <optional: section-kind> section-name: <section-caption>
-                return match (parts.next(), parts.next()) {
-                    (Some(_kind), Some(_name)) => true,
-                    (Some(_name), None) if _name.contains("youtube") => true,
-                    (_, _) => false,
-                };
-            }
-
-            false
-        }
-
-        /// returns true if the line is commented or escaped out section, otherwise false
-        fn is_section_commented_or_escaped(line: &str) -> bool {
-            line.starts_with("/-- ") || line.starts_with(r"\-- ")
-        }
-
-        /// returns true if the line is commented out section/subsection, otherwise false
-        fn is_line_commented(line: &str) -> bool {
-            line.starts_with("/-- ") || line.starts_with("/--- ")
-        }
-
-        /// returns true if the line is escaped out section/subsection, otherwise false
-        fn is_line_escaped(line: &str) -> bool {
-            line.starts_with(r"\-- ") || line.starts_with(r"\--- ")
-        }
-
-        // id: <some-id>
-        // <some-id> could be any string using this character set
-        // character set = [A-Z, a-z, 0-9, whitespace, '_' , '-' ]
-        // leading and trailing whitespace characters are ignored
-        const ID_HEADER_PATTERN: &str = r"(?m)^\s*id\s*:[-_\sA-Za-z\d]*$";
-        lazy_static::lazy_static!(
-            static ref ID: regex::Regex = regex::Regex::new(ID_HEADER_PATTERN).unwrap();
-        );
-
-        // Flags to ignore grepping for id under certain cases
-        let mut ignore_id: bool = true;
-        let mut register_id_for_last_section: bool = false;
-
-        // grep all lines where user defined `id` for the sections/ subsecions
-        // and update the global_ids map
-        for (ln, line) in itertools::enumerate(data.lines()) {
-            // Trim leading whitespaces if any
-            let line = line.trim_start();
-
-            // Ignore for commented out section/subsection
-            // and for ftd code passed down as body to ft.code
-            if is_line_commented(line) || is_line_escaped(line) {
-                if is_section_commented_or_escaped(line) {
-                    register_id_for_last_section = false;
-                }
-                ignore_id = true;
-            }
-
-            // section could be component definition
-            // in that case ignore relevant id's defined under its definition
-            // including the ids defined on its subsections
-            if ftd::identifier::is_section(line) {
-                ignore_id = ignore_next_id(line);
-                register_id_for_last_section = !ignore_id;
-            }
-
-            // In cases, where youtube component is used
-            // within an invoked section/ container as a subsection
-            // or when there are uncommented subsections
-            // under commented section then ignore their id's
-            if ftd::identifier::is_subsection(line) && register_id_for_last_section {
-                ignore_id = ignore_next_id(line);
-            }
-
-            // match for id and update the id map (if found)
-            if !ignore_id && ID.is_match(line) {
-                update_id_map(&mut self.global_ids, line, doc_id, ln)?;
-            }
-
-            // In case if we want to
-            // Avoid using regex here to reduce complexity as the pattern
-            // to search itself is not that complex
-            // ^id: <some-alphanumeric-string>$
-            // if line.trim_start().starts_with("term") && line.contains(':') && !ignore_id{
-            //     update_terms(&mut self.global_ids, line.trim_start(), doc_id, ln)?;
-            // }
+        // Vec<captured_id, line_number>
+        let captured_global_ids: Vec<(String, usize)> = ftd::p1::parse_file_for_global_ids(data);
+        for (captured_id, ln) in captured_global_ids.iter() {
+            update_id_map(&mut self.global_ids, captured_id.as_str(), doc_id, *ln)?;
         }
 
         Ok(())
