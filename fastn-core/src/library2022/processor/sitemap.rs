7dd9460f39b3e228ea1aa97e8e433ec6d6793da5
heulitig <rithik@fifthtry.com>
2023-02-16 16:44:30 +0530

    updated is-active for full-sitemap

--- fastn-core/src/library2022/processor/sitemap.rs
+++ fastn-core/src/library2022/processor/sitemap.rs
@@ -124,6 +124,17 @@ pub fn to_sitemap_compat(
         toc_item: &fastn_core::sitemap::toc::TocItem,
         current_document: &str,
     ) -> fastn_core::sitemap::toc::TocItemCompat {
+
+        let mut is_child_active: bool = false;
+        let mut children: Vec<fastn_core::sitemap::toc::TocItemCompat> = vec![];
+        for child in toc_item.children.iter().filter(|t| !t.skip) {
+            let child_to_toc_compat = to_toc_compat(child, current_document);
+            if child_to_toc_compat.is_active {
+                is_child_active = true;
+            }
+            children.push(child_to_toc_compat);
+        }
+
         let toc_compat = fastn_core::sitemap::toc::TocItemCompat {
             url: Some(toc_item.id.clone()),
             number: None,
@@ -133,15 +144,10 @@ pub fn to_sitemap_compat(
             font_icon: toc_item.icon.clone().map(|v| v.into()),
             bury: toc_item.bury,
             extra_data: toc_item.extra_data.to_owned(),
-            is_active: fastn_core::utils::ids_matches(toc_item.id.as_str(), current_document),
+            is_active: fastn_core::utils::ids_matches(toc_item.id.as_str(), current_document) || is_child_active,
             is_open: false,
             nav_title: toc_item.nav_title.clone(),
-            children: toc_item
-                .children
-                .iter()
-                .filter(|t| !t.skip)
-                .map(|t| to_toc_compat(t, current_document))
-                .collect_vec(),
+            children,
             readers: toc_item.readers.clone(),
             writers: toc_item.writers.clone(),
             is_disabled: false,
@@ -155,6 +161,17 @@ pub fn to_sitemap_compat(
         subsection: &fastn_core::sitemap::section::Subsection,
         current_document: &str,
     ) -> fastn_core::sitemap::toc::TocItemCompat {
+
+        let mut is_child_active: bool = false;
+        let mut children: Vec<fastn_core::sitemap::toc::TocItemCompat> = vec![];
+        for child in subsection.toc.iter().filter(|t| !t.skip) {
+            let child_to_toc_compat = to_toc_compat(child, current_document);
+            if child_to_toc_compat.is_active {
+                is_child_active = true;
+            }
+            children.push(child_to_toc_compat);
+        }
+
         fastn_core::sitemap::toc::TocItemCompat {
             url: subsection.id.clone(),
             title: subsection.title.clone(),
@@ -164,18 +181,14 @@ pub fn to_sitemap_compat(
             bury: subsection.bury,
             extra_data: subsection.extra_data.to_owned(),
             is_active: if let Some(ref subsection_id) = subsection.id {
-                fastn_core::utils::ids_matches(subsection_id.as_str(), current_document)
+                fastn_core::utils::ids_matches(subsection_id.as_str(), current_document) || is_child_active
             } else {
-                false
+                is_child_active
             },
             is_open: false,
             image_src: None,
             nav_title: subsection.nav_title.clone(),
-            children: subsection
-                .toc
-                .iter()
-                .map(|t| to_toc_compat(t, current_document))
-                .collect_vec(),
+            children,
             readers: subsection.readers.clone(),
             writers: subsection.writers.clone(),
             number: None,
@@ -188,6 +201,17 @@ pub fn to_sitemap_compat(
         section: &fastn_core::sitemap::section::Section,
         current_document: &str,
     ) -> fastn_core::sitemap::toc::TocItemCompat {
+
+        let mut is_child_active: bool = false;
+        let mut children: Vec<fastn_core::sitemap::toc::TocItemCompat> = vec![];
+        for child in section.subsections.iter().filter(|t| !t.skip) {
+            let child_to_subsection_compat = to_subsection_compat(child, current_document);
+            if child_to_subsection_compat.is_active {
+                is_child_active = true;
+            }
+            children.push(child_to_subsection_compat);
+        }
+
         fastn_core::sitemap::toc::TocItemCompat {
             url: Some(section.id.to_string()),
             number: None,
@@ -197,16 +221,10 @@ pub fn to_sitemap_compat(
             font_icon: section.icon.clone().map(|v| v.into()),
             bury: section.bury,
             extra_data: section.extra_data.to_owned(),
-            is_active: fastn_core::utils::ids_matches(section.id.as_str(), current_document),
+            is_active: { fastn_core::utils::ids_matches(section.id.as_str(), current_document) || is_child_active },
             is_open: false,
             nav_title: section.nav_title.clone(),
-            children: section
-                .subsections
-                .iter()
-                .filter(|s| !s.skip)
-                .filter(|s| s.visible)
-                .map(|s| to_subsection_compat(s, current_document))
-                .collect_vec(),
+            children,
             readers: section.readers.clone(),
             writers: section.writers.clone(),
             is_disabled: false,
